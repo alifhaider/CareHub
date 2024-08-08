da48b9552fc9a51f43a420e7b679de8eabb2d577
alif haider <alifhaider57@gmail.com>
2024-08-09 05:08:14 +0600

    display prev month dates

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -1,11 +1,30 @@
-import { StepBackIcon, StepForwardIcon } from "lucide-react";
+import { useState } from "react";
 
 function getShortendText(text: string, length: number = 3) {
   return text.slice(0, length);
 }
 
+function getDatesInView(
+  currentDate: number,
+  numberOfDays: number,
+  currentDay: number,
+  numberOfDaysInPrevMonth: number,
+  numberOfDaysInNextMonth: number,
+) {
+  const extraSlotsForPrevMonth = currentDate % currentDay
+  // const extraSlotsForNextMonth = 
+  const lastMonthDates = getPrevMonthDates(extraSlotsForPrevMonth, numberOfDaysInPrevMonth)
+  // const nextMonthDates = getNextMonthDates(extraSlots, numberOfDaysInNextMonth)
+  const dates = [...lastMonthDates];
+  for (let i = 1; i <= numberOfDays; i++) {
+    dates.push(i);
+  }
+  return dates;
+}
 
-function startDayFrom(currentDate: number) {}
+function getPrevMonthDates(extraSlots: number, numberOfDaysInPrevMonth: number) {
+  return Array.from({length: numberOfDaysInPrevMonth}).map((_, i) => i + 1).splice(numberOfDaysInPrevMonth - extraSlots, numberOfDaysInPrevMonth)
+}
 
 export default function Calendar() {
   const days = [
@@ -32,12 +51,15 @@ export default function Calendar() {
     "December",
   ];
   const dateRange = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const [date, setDate] = useState(() => new Date())
 
-  const today = new Date();
-  const currentDate = today.getDate();
-  const currentMonth = today.getMonth();
-  const currentYear = today.getFullYear();
-  const currentDay = today.getDay();
+  const today = new Date(); // August 9th, 2024 (Friday)
+  const currentDate = today.getDate(); // returns the date 9
+  const currentMonth = today.getMonth(); // 7
+  const currentYear = today.getFullYear(); // 2024
+  const currentDay = today.getDay(); // 5 -> starts from sunday
+
+  const datesInView = getDatesInView(currentDate, dateRange[currentMonth], currentDay, dateRange[currentMonth -1], dateRange[currentMonth + 1])
 
   return (
     <div className="max-w-3xl border rounded-lg p-2">
@@ -58,20 +80,31 @@ export default function Calendar() {
         <Weeks days={days} currentDay={currentDay} />
 
         <ul className="grid grid-cols-7">
-          {Array.from({ length: dateRange[currentMonth] }).map((_, index) => {
-            return (
-              <li
-                key={index}
-                className={`text-center py-2 ${
-                  index + 1 === currentDate
-                    ? "text-primary bg-secondary font-medium underline"
-                    : "text-muted-foreground"
-                } ${index + 1 < currentDate ? "line-through" : ""}`}
-              >
-                <button className="w-full h-full">{index + 1}</button>
-              </li>
-            );
-          })}
+          {datesInView.map(
+            (date, index) => {
+              return (
+                <li
+                  key={index}
+                  className={`text-center py-2 ${
+                    date === currentDate
+                      ? "text-primary bg-secondary font-medium underline"
+                      : "text-muted-foreground"
+                  }`}
+                >
+                  {date < currentDate ? (
+                    <span
+                      title="Past days are not allowed"
+                      className="cursor-not-allowed line-through"
+                    >
+                      {date}
+                    </span>
+                  ) : (
+                    <button className="w-full h-full">{date}</button>
+                  )}
+                </li>
+              );
+            }
+          )}
         </ul>
       </div>
     </div>
@@ -95,7 +128,7 @@ const Weeks = ({ days, currentDay }: WeeksProps) => {
               : "text-muted-foreground"
           }`}
         >
-          {day}
+          {getShortendText(day)}
         </li>
       ))}
     </ul>
