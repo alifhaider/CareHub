088223f96679387df9c6bcca605dfbbb853b85df
alif haider <alifhaider57@gmail.com>
2024-08-09 05:32:08 +0600

    slots for next month dates

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -1,4 +1,30 @@
-import { useState } from "react";
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const DAYS_PER_WEEK = 7
+
+const DATE_RANGES = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function getShortendText(text: string, length: number = 3) {
   return text.slice(0, length);
@@ -6,52 +32,46 @@ function getShortendText(text: string, length: number = 3) {
 
 function getDatesInView(
   currentDate: number,
-  numberOfDays: number,
+  numberOfDaysInCurrentMonth: number,
   currentDay: number,
   numberOfDaysInPrevMonth: number,
-  numberOfDaysInNextMonth: number,
+  numberOfDaysInNextMonth: number
 ) {
-  const extraSlotsForPrevMonth = currentDate % currentDay
-  // const extraSlotsForNextMonth = 
-  const lastMonthDates = getPrevMonthDates(extraSlotsForPrevMonth, numberOfDaysInPrevMonth)
-  // const nextMonthDates = getNextMonthDates(extraSlots, numberOfDaysInNextMonth)
-  const dates = [...lastMonthDates];
-  for (let i = 1; i <= numberOfDays; i++) {
-    dates.push(i);
-  }
-  return dates;
+  const extraSlotsForPrevMonth = currentDate % currentDay;
+  const extraSlotsForNextMonth = (extraSlotsForPrevMonth + numberOfDaysInCurrentMonth) % DAYS_PER_WEEK;
+  const prevMonthDates = getPrevMonthDates(
+    extraSlotsForPrevMonth,
+    numberOfDaysInPrevMonth
+  );
+  const nextMonthDates = getNextMonthDates(
+    extraSlotsForNextMonth,
+    numberOfDaysInNextMonth
+  );
+
+  const dates = Array.from({ length: numberOfDaysInCurrentMonth }).map((_, i) => i + 1);
+  return { prevMonthDates, currentMonthDates: dates, nextMonthDates };
 }
 
-function getPrevMonthDates(extraSlots: number, numberOfDaysInPrevMonth: number) {
-  return Array.from({length: numberOfDaysInPrevMonth}).map((_, i) => i + 1).splice(numberOfDaysInPrevMonth - extraSlots, numberOfDaysInPrevMonth)
+function getPrevMonthDates(
+  extraSlots: number,
+  numberOfDaysInPrevMonth: number
+) {
+  return Array.from({ length: numberOfDaysInPrevMonth })
+    .map((_, i) => i + 1)
+    .splice(numberOfDaysInPrevMonth - extraSlots, numberOfDaysInPrevMonth);
+}
+
+function getNextMonthDates(
+  extraSlotsForNextMonth: number,
+  numberOfDaysInNextMonth: number
+) {
+  return Array.from({ length: numberOfDaysInNextMonth })
+    .map((_, i) => i + 1)
+    .splice(0, extraSlotsForNextMonth);
 }
 
 export default function Calendar() {
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const dateRange = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  const [date, setDate] = useState(() => new Date())
+  // const [date, setDate] = useState(() => new Date())
 
   const today = new Date(); // August 9th, 2024 (Friday)
   const currentDate = today.getDate(); // returns the date 9
@@ -59,7 +79,13 @@ export default function Calendar() {
   const currentYear = today.getFullYear(); // 2024
   const currentDay = today.getDay(); // 5 -> starts from sunday
 
-  const datesInView = getDatesInView(currentDate, dateRange[currentMonth], currentDay, dateRange[currentMonth -1], dateRange[currentMonth + 1])
+  const { prevMonthDates, currentMonthDates, nextMonthDates } = getDatesInView(
+    currentDate,
+    DATE_RANGES[currentMonth],
+    currentDay,
+    DATE_RANGES[currentMonth - 1],
+    DATE_RANGES[currentMonth + 1]
+  );
 
   return (
     <div className="max-w-3xl border rounded-lg p-2">
@@ -67,44 +93,61 @@ export default function Calendar() {
         <button>{currentYear - 1}</button>
         <div className="flex items-center justify-center gap-4">
           <button className="text-muted-foreground text-xs">
-            {getShortendText(months[currentMonth - 1])}
+            {getShortendText(MONTHS[currentMonth - 1])}
           </button>
-          <span className="text-primary">{months[currentMonth]}</span>
+          <span className="text-primary">{MONTHS[currentMonth]}</span>
           <button className="text-muted-foreground text-xs">
-            {getShortendText(months[currentMonth + 1])}
+            {getShortendText(MONTHS[currentMonth + 1])}
           </button>
         </div>
         <button>{currentYear + 1}</button>
       </div>
       <div className="text-sm mt-4">
-        <Weeks days={days} currentDay={currentDay} />
+        <Weeks days={DAYS} currentDay={currentDay} />
 
         <ul className="grid grid-cols-7">
-          {datesInView.map(
-            (date, index) => {
-              return (
-                <li
-                  key={index}
-                  className={`text-center py-2 ${
-                    date === currentDate
-                      ? "text-primary bg-secondary font-medium underline"
-                      : "text-muted-foreground"
-                  }`}
-                >
-                  {date < currentDate ? (
-                    <span
-                      title="Past days are not allowed"
-                      className="cursor-not-allowed line-through"
-                    >
-                      {date}
-                    </span>
-                  ) : (
-                    <button className="w-full h-full">{date}</button>
-                  )}
-                </li>
-              );
-            }
-          )}
+          {prevMonthDates.map((date, index) => {
+            return (
+              <li
+                key={index}
+                className="text-center py-2 text-muted-foreground"
+              >
+                <PrevDate date={date} />
+              </li>
+            );
+          })}
+          {currentMonthDates.map((date, index) => {
+            return (
+              <li
+                key={index}
+                className={`text-center py-2 ${
+                  date === currentDate
+                    ? "text-primary bg-secondary font-medium underline"
+                    : "text-muted-foreground"
+                }`}
+              >
+                {date < currentDate ? (
+                  <PrevDate date={date} />
+                ) : (
+                  <button className="w-full h-full">{date}</button>
+                )}
+              </li>
+            );
+          })}
+          {nextMonthDates.map((date, index) => {
+            return (
+              <li
+                key={index}
+                className={`text-center py-2 ${
+                  date === currentDate
+                    ? "text-primary bg-secondary font-medium underline"
+                    : "text-muted-foreground"
+                }`}
+              >
+                <button className="w-full h-full">{date}</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
@@ -132,5 +175,16 @@ const Weeks = ({ days, currentDay }: WeeksProps) => {
         </li>
       ))}
     </ul>
+  );
+};
+
+const PrevDate = ({ date }: { date: number }) => {
+  return (
+    <span
+      title="Past days are not allowed"
+      className="cursor-not-allowed line-through text-destructive"
+    >
+      {date}
+    </span>
   );
 };
