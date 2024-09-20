78d444189db6035c2b4380828749c9cceb68369f
alif haider <alifhaider57@gmail.com>
2024-09-21 02:18:29 +0600

    feat: no "useState" on onboarding

--- app/routes/doctor.onboarding.tsx
+++ app/routes/doctor.onboarding.tsx
@@ -1,8 +1,7 @@
 import { Form, MetaFunction, useActionData } from '@remix-run/react'
 import { Plus, Trash2 } from 'lucide-react'
-import { useState } from 'react'
-import { Field, TextareaField } from '~/components/forms'
-import { Button } from '~/components/ui/button'
+import { ErrorList, Field, TextareaField } from '~/components/forms'
+import { Button, buttonVariants } from '~/components/ui/button'
 import { AnimatePresence, motion } from 'framer-motion'
 import {
   Card,
@@ -14,144 +13,75 @@ import {
 import { z } from 'zod'
 import { ActionFunctionArgs, json, redirect } from '@remix-run/node'
 import { parseWithZod } from '@conform-to/zod'
-import { getFormProps, getInputProps, useForm } from '@conform-to/react'
-
-interface FieldProps {
-  id: number
-}
-
-interface EducationField extends FieldProps {
-  degree: string
-  institute: string
-  passedYear: string
-}
-
-interface SpecialtyField extends FieldProps {
-  name: string
-}
-
-type AnimatedFieldProps = {
-  children: React.ReactNode
-}
-
-interface FieldSectionProps<T extends FieldProps> {
-  title: string
-  fields: T[]
-  addField: () => void
-  renderField: (field: T) => React.ReactNode
-}
+import {
+  getFieldsetProps,
+  getFormProps,
+  getInputProps,
+  useForm,
+} from '@conform-to/react'
+import { cn } from '~/lib/utils'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Onboarding / CH' }]
 }
 
+const EducationSchema = z.object({
+  degree: z.string({ message: 'Add degree' }),
+  institute: z.string({ message: 'Add institute' }),
+  passedYear: z.string({ message: 'Add passed year' }),
+})
+
+const SpecialtySchema = z.object({
+  name: z.string({ message: 'Add specialty' }),
+})
+
 const OnboardingSchema = z.object({
-  fullName: z.string(),
-  phoneNumber: z.string(),
-  education: z.array(
-    z.object({
-      degree: z.string(),
-      institute: z.string(),
-      passedYear: z.string(),
+  fullName: z.string({ message: 'Full name is required(ex: Dr. John Doe)' }),
+  phoneNumber: z.string().optional(),
+  educations: z
+    .array(EducationSchema)
+    .nonempty({ message: 'Add at least one education (ex: MBBS, MD)' }),
+  specialties: z
+    .array(SpecialtySchema)
+    .nonempty({
+      message: 'Add at least one specialty (ex: Cardiology, Dermatology)',
     }),
-  ),
-  specialties: z.array(z.object({ name: z.string() })),
-  bio: z.string(),
-  profilePicture: z.string(),
+  bio: z.string().optional(),
+  profilePicture: z.string().optional(),
 })
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData()
-
-  const submission = await parseWithZod(formData, {
-    schema: () =>
-      OnboardingSchema.transform(async (data, ctx) => {
-        const doctor = { id: 1 } // perform doctor onboarding here
-
-        if (!doctor) {
-          ctx.addIssue({
-            code: 'custom',
-            message: 'Could not create doctor',
-          })
-          return z.NEVER
-        }
-
-        return { ...data, doctor }
-      }),
-    async: true,
+  const submission = parseWithZod(formData, {
+    schema: OnboardingSchema,
   })
 
   if (submission.status !== 'success') {
     return json(submission.reply({ formErrors: ['Could not onboard doctor'] }))
   }
 
+  const doctor = { id: 1 } // perform doctor onboarding here
+
   return redirect('/')
 }
 
-function FieldSection<T extends FieldProps>({
-  title,
-  fields,
-  addField,
-  renderField,
-}: FieldSectionProps<T>) {
-  return (
-    <div>
-      <h3 className="mb-2 text-lg font-bold">{title}</h3>
-      <AnimatePresence initial={false}>
-        {fields.map(renderField)}
-      </AnimatePresence>
-      <Button type="button" variant="outline" size="sm" onClick={addField}>
-        <Plus className="mr-2 h-4 w-4" /> Add {title}
-      </Button>
-    </div>
-  )
-}
-
-const AnimatedField = ({ children }: AnimatedFieldProps) => (
-  <motion.div
-    initial={{ opacity: 0, height: 0 }}
-    animate={{ opacity: 1, height: 'auto' }}
-    exit={{ opacity: 0, height: 0 }}
-    transition={{ opacity: { duration: 0.2 }, height: { duration: 0.3 } }}
-  >
-    {children}
-  </motion.div>
-)
-
 export default function DoctorOnboarding() {
-  const actionData = useActionData<typeof action>()
+  const lastResult = useActionData<typeof action>()
 
   const [form, fields] = useForm({
-    lastResult: actionData,
+    lastResult,
     onValidate({ formData }) {
       return parseWithZod(formData, { schema: OnboardingSchema })
     },
     shouldRevalidate: 'onSubmit',
+    defaultValue: {
+      educations: [{ degree: '', institute: '', passedYear: '' }],
+      specialties: [{ name: '' }],
+    },
   })
 
-  const [educationFields, setEducationFields] = useState([
-    { id: Date.now(), degree: '', institute: '', passedYear: '' },
-  ])
-  const [specialtyFields, setSpecialtyFields] = useState([
-    { id: Date.now(), name: '' },
-  ])
-
-  const addField = <T extends FieldProps>(
-    setFields: React.Dispatch<React.SetStateAction<T[]>>,
-    newField: Omit<T, 'id' | 'isNew'>,
-  ) => {
-    setFields(prev => [...prev, { ...newField, id: Date.now() } as T])
-  }
-
-  const removeField = <T extends FieldProps>(
-    setFields: React.Dispatch<React.SetStateAction<T[]>>,
-    id: number,
-  ) => {
-    setFields(prev => prev.filter(field => field.id !== id))
-  }
-
-  const educations = fields.education.getFieldList()
-  console.log({educations})
+  const educations = fields.educations.getFieldList()
+  const specialties = fields.specialties.getFieldList()
 
   return (
     <div className="min-h-screen bg-background p-8">
@@ -168,9 +98,9 @@ export default function DoctorOnboarding() {
               <Field
                 labelProps={{ children: 'Full Name' }}
                 inputProps={{
-                  placeholder: 'Dr. John Doe',
                   ...getInputProps(fields.fullName, { type: 'text' }),
                 }}
+                errors={fields.fullName.errors}
               />
 
               <Field
@@ -179,6 +109,7 @@ export default function DoctorOnboarding() {
                   placeholder: '+1234567890',
                   ...getInputProps(fields.phoneNumber, { type: 'tel' }),
                 }}
+                errors={fields.phoneNumber.errors}
               />
             </div>
 
@@ -187,6 +118,93 @@ export default function DoctorOnboarding() {
               <AnimatePresence initial={false}>
                 {educations.map((education, index) => {
                   const educationFields = education.getFieldset()
+                  return (
+                    <motion.div
+                      key={education.key}
+                      initial={{ opacity: 0, height: 0 }}
+                      animate={{ opacity: 1, height: 'auto' }}
+                      exit={{ opacity: 0, height: 0 }}
+                      transition={{
+                        opacity: { duration: 0.2 },
+                        height: { duration: 0.3 },
+                      }}
+                    >
+                      <fieldset
+                        key={education.key}
+                        {...getFieldsetProps(education)}
+                      >
+                        <div className="grid grid-cols-9 items-center gap-4">
+                          <Field
+                            className="col-span-4"
+                            labelProps={{ children: 'Institute' }}
+                            inputProps={{
+                              ...getInputProps(educationFields.institute, {
+                                type: 'text',
+                              }),
+                            }}
+                            errors={educationFields.institute.errors}
+                          />
+                          <Field
+                            className="col-span-2"
+                            labelProps={{ children: 'Degree' }}
+                            inputProps={{
+                              ...getInputProps(educationFields.degree, {
+                                type: 'text',
+                              }),
+                            }}
+                            errors={educationFields.degree.errors}
+                          />
+                          <Field
+                            className="col-span-2"
+                            labelProps={{ children: 'Passed Year' }}
+                            inputProps={{
+                              ...getInputProps(educationFields.passedYear, {
+                                type: 'text',
+                              }),
+                            }}
+                            errors={educationFields.passedYear.errors}
+                          />
+                          <button
+                            className={cn(
+                              buttonVariants({
+                                variant: 'destructive',
+                                size: 'icon',
+                              }),
+                              'mb-[10px]',
+                            )}
+                            {...form.remove.getButtonProps({
+                              name: fields.educations.name,
+                              index,
+                            })}
+                          >
+                            <Trash2 className="h-4 w-4" />
+                          </button>
+                        </div>
+                      </fieldset>
+                    </motion.div>
+                  )
+                })}
+              </AnimatePresence>
+
+              <ErrorList errors={fields.educations.errors} />
+
+              <button
+                className={cn(
+                  buttonVariants({ variant: 'outline', size: 'sm' }),
+                )}
+                {...form.insert.getButtonProps({
+                  name: fields.educations.name,
+                })}
+              >
+                <Plus className="mr-2 h-4 w-4" /> Add Education
+              </button>
+            </div>
+
+            <div className="pt-8">
+              <h3 className="mb-2 text-lg font-bold">Specialties</h3>
+              <AnimatePresence initial={false}>
+                {specialties.map((specialty, index) => {
+                  const specialtyFields = specialty.getFieldset()
                   return (
                     <motion.div
                       key={index}
@@ -198,158 +216,54 @@ export default function DoctorOnboarding() {
                         height: { duration: 0.3 },
                       }}
                     >
-                      <div className="grid grid-cols-9 items-center gap-4">
-                        <Field
-                          className="col-span-4"
-                          labelProps={{ children: 'Institute' }}
-                          inputProps={{
-                            placeholder:
-                              'University of California, San Francisco',
-                            ...getInputProps(educationFields.institute, {
-                              type: 'text',
-                            }),
-                          }}
-                        />
-                        <Field
-                          className="col-span-2"
-                          labelProps={{ children: 'Degree' }}
-                          inputProps={{
-                            placeholder: 'MD',
-                            ...getInputProps(educationFields.degree, {
-                              type: 'text',
-                            }),
-                          }}
-                        />
-                        <Field
-                          className="col-span-2"
-                          labelProps={{ children: 'Passed Year' }}
-                          inputProps={{
-                            placeholder: '2024',
-                            ...getInputProps(educationFields.passedYear, {
-                              type: 'text',
-                            }),
-                          }}
-                        />
-                        <Button
-                          type="button"
-                          variant="destructive"
-                          className="mb-[10px]"
-                          size="icon"
-                          // onClick={() =>
-                          //   removeField(setEducationFields, education.id)
-                          // }
-                        >
-                          <Trash2 className="h-4 w-4" />
-                        </Button>
-                      </div>
+                      <fieldset
+                        key={specialty.key}
+                        {...getFieldsetProps(specialty)}
+                      >
+                        <div className="grid grid-cols-9 items-center gap-4">
+                          <Field
+                            className="col-span-4"
+                            labelProps={{ children: 'Name' }}
+                            inputProps={{
+                              ...getInputProps(specialtyFields.name, {
+                                type: 'text',
+                              }),
+                            }}
+                            errors={specialtyFields.name.errors}
+                          />
+                          <button
+                            {...form.remove.getButtonProps({
+                              name: fields.specialties.name,
+                              index,
+                            })}
+                            className={cn(
+                              buttonVariants({
+                                variant: 'destructive',
+                                size: 'icon',
+                              }),
+                              'mb-[10px]',
+                            )}
+                          >
+                            <Trash2 className="h-4 w-4" />
+                          </button>
+                        </div>
+                      </fieldset>
                     </motion.div>
                   )
                 })}
               </AnimatePresence>
+              <ErrorList errors={fields.specialties.errors} />
 
-              <Button
-                type="button"
-                variant="outline"
-                size="sm"
-                // onClick={() =>
-                //   addField(educations.addField, {
-                //     degree: '',
-                //     institute: '',
-                //     passedYear: '',
-                //   })
-                // }
+              <button
+                {...form.insert.getButtonProps({
+                  name: fields.specialties.name,
+                })}
+                className={cn(
+                  buttonVariants({ variant: 'outline', size: 'sm' }),
+                )}
               >
-                <Plus className="mr-2 h-4 w-4" /> Add Education
-              </Button>
-
-              {/* <FieldSection<EducationField>
-                title="Education"
-                fields={educationFields}
-                addField={() =>
-                  addField(setEducationFields, {
-                    degree: '',
-                    institute: '',
-                    passedYear: '',
-                  })
-                }
-                renderField={education => (
-                  <AnimatedField key={education.id}>
-                    <div className="grid grid-cols-9 items-center gap-4">
-                      <Field
-                        className="col-span-4"
-                        labelProps={{ children: 'Institute' }}
-                        inputProps={{
-
-                          placeholder:
-                            'University of California, San Francisco',
-                          ...getInputProps(fields.education., { type: 'text' }),
-                        }}
-                      />
-                      <Field
-                        className="col-span-2"
-                        labelProps={{ children: 'Degree' }}
-                        inputProps={{
-                          type: 'text',
-                          placeholder: 'MD',
-                        }}
-                      />
-                      <Field
-                        className="col-span-2"
-                        labelProps={{ children: 'Passed Year' }}
-                        inputProps={{
-                          type: 'text',
-                          placeholder: '2024',
-                          value: education.passedYear,
-                        }}
-                      />
-                      <Button
-                        type="button"
-                        variant="destructive"
-                        className="mb-[10px]"
-                        size="icon"
-                        onClick={() =>
-                          removeField(setEducationFields, education.id)
-                        }
-                      >
-                        <Trash2 className="h-4 w-4" />
-                      </Button>
-                    </div>
-                  </AnimatedField>
-                )}
-              /> */}
-            </div>
-
-            {/* Specialties Section */}
-            <div className="pt-8">
-              <FieldSection<SpecialtyField>
-                title="Specialties"
-                fields={specialtyFields}
-                addField={() => addField(setSpecialtyFields, { name: '' })}
-                renderField={field => (
-                  <AnimatedField key={field.id}>
-                    <div className="flex items-center gap-4">
-                      <Field
-                        labelProps={{ children: 'Specialty' }}
-                        inputProps={{
-                          type: 'text',
-                          placeholder: 'Cardiology',
-                        }}
-                      />
-                      <Button
-                        type="button"
-                        variant="destructive"
-                        size="icon"
-                        className="mb-[9px]"
-                        onClick={() =>
-                          removeField(setSpecialtyFields, field.id)
-                        }
-                      >
-                        <Trash2 className="h-4 w-4" />
-                      </Button>
-                    </div>
-                  </AnimatedField>
-                )}
-              />
+                <Plus className="mr-2 h-4 w-4" /> Add specialty
+              </button>
             </div>
 
             <TextareaField
@@ -358,12 +272,14 @@ export default function DoctorOnboarding() {
                 placeholder:
                   'Tell us about your experience and approach to patient care...',
               }}
+              errors={fields.bio.errors}
             />
 
             <Field
               labelProps={{ children: 'Profile Picture' }}
               inputProps={{ type: 'file' }}
               className="mb-4 max-w-xs"
+              errors={fields.profilePicture.errors}
             />
 
             <Button type="submit" className="w-full">
